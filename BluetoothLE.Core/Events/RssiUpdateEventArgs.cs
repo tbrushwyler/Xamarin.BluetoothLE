75300af3b43b493ef1a7e59e06d20be2bd5cd4c9
Tobias Boogh <tobias.boogh@bybrick.se>
2016-06-07 09:33:34 +0200

    Added missing file

---
+++ BluetoothLE.Core/Events/RssiUpdateEventArgs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BluetoothLE.Core.Events {
+	public class RssiUpdateEventArgs : EventArgs{
+		public int Rssi { get; private set; }
+
+		public RssiUpdateEventArgs(int rssi) {
+			Rssi = rssi;
+		}
+	}
+}
