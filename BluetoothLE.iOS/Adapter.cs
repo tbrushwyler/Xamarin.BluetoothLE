73a7f65d82ac6ac4da5f9c526937dec12ae9ca47
Tobias Boogh <tobias.boogh@bybrick.se>
2016-06-23 10:57:52 +0200

    Added null check

--- BluetoothLE.iOS/Adapter.cs
+++ BluetoothLE.iOS/Adapter.cs
@@ -193,7 +193,10 @@ namespace BluetoothLE.iOS {
 			await Task.Delay(ConnectionTimeout);
 
 			if (ConnectedDevices.All(x => x.Id != device.Id)) {
-				_central.CancelPeripheralConnection(peripheral);
+                if (peripheral != null) {
+                    _central.CancelPeripheralConnection(peripheral);
+                }
+                
 				var args = new DeviceConnectionEventArgs(device) {
 					ErrorMessage = "The device connection timed out."
 				};
